18b31e94cffaa9b6b613ce591451caa771d546ff
Wesley Rodrigues <wesleyrodriguesmoreira02@gmail.com>
2024-10-17 00:25:55 -0300

    3° commit

--- ci4-project/app/Config/Routes.php
+++ ci4-project/app/Config/Routes.php
@@ -5,4 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+$routes->group('teste', ['namespace' => 'App\Controllers\Teste'], static function($routes){
+    $routes->get('/', 'Home::index', ['e' => 'teste_01']);
+    $routes->get('vedor', 'Home::view', ['o' => 'teste_02']);
+    
+});
+ 
 $routes->get('/', 'Home::index');
+$routes->get('/testePost', 'Home::view', ['as' => 'teste_post']);
